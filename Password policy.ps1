b2e8a1d238e1edf9fe96068dcee09cb5ad3155de
HaskellKnight <54657097+HaskellKnight@users.noreply.github.com>
2019-08-29 21:17:22 -0700

    Add files via upload

---
+++ Password policy.ps1
@@ -0,0 +1,2 @@
+﻿Set-ADDefaultDomainPasswordPolicy -PasswordHistoryCount 24 -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -MinPasswordLength 10 -ComplexityEnabled $True -ReversibleEncryptionEnabled $False
+
