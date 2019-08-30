a0d8bf43a33c74540df4b5f15cbc25a91ef1bf6e
HaskellKnight <54657097+HaskellKnight@users.noreply.github.com>
2019-08-29 21:46:05 -0700

    Update Password policy.ps1

--- Password policy.ps1
+++ Password policy.ps1
@@ -1,2 +1,13 @@
-﻿Set-ADDefaultDomainPasswordPolicy -PasswordHistoryCount 24 -MaxPasswordAge 60.00:00:00 -MinPasswordAge 1.00:00:00 -MinPasswordLength 10 -ComplexityEnabled $True -ReversibleEncryptionEnabled $False
+#Password Policy
+Set-ADDefaultDomainPasswordPolicy -PasswordHistoryCount 24 
+Set-ADDefaultDomainPasswordPolicy -MaxPasswordAge 60.00:00:00 
+Set-ADDefaultDomainPasswordPolicy -MinPasswordAge 1.00:00:00 
+Set-ADDefaultDomainPasswordPolicy -MinPasswordLength 10 
+Set-ADDefaultDomainPasswordPolicy -ComplexityEnabled $True 
+Set-ADDefaultDomainPasswordPolicy -ReversibleEncryptionEnabled $False
+
+#Account Lockout Policy
+Set-ADDefaultDomainPasswordPolicy -LockoutThreshold 10 
+Set-ADDefaultDomainPasswordPolicy -LockoutDuration 00:30:00 
+Set-ADDefaultDomainPasswordPolicy -LockoutObservationWindow 00:30:00
 
